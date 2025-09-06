8d1f8e6a4a054308d674494aa469a6211ddcc987
kv4sman <xod3801@gmail.com>
2025-09-07 00:29:04 +0300

    eto kod

--- pvio.cpp
+++ pvio.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <cstring>
 
-const int mod = 1e9+7;
+const int mod = 2e9+7;
 
 signed main(){
     std::ios_base::sync_with_stdio(0); std::cin.tie(0);
@@ -12,8 +12,22 @@ signed main(){
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int a;
-    int b;
-    std::cin >> a >> b;
-    std::cout << a + b;
+    int min1 = mod;
+    int min2 = mod;
+    std::cin >> a;
+    int mas[a+10];
+    for (int i = 0; i < a; i++){
+        std::cin >> mas[i];
+    }
+
+    for (int i = 0; i < a; i++){
+        if (mas[i] < min1) {
+            min2 = min1;
+            min1 = mas[i];
+        }
+    }
+    
+    std::cout << min1 << " " << min2;
+    
     return 0;
 }
